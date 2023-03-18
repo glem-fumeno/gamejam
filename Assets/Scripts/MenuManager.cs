9cb1721e38f2369cea9747d2978c166c687d373e
Emilia Glibowska <emilia.glibowska@elmark.com.pl>
2023-03-18 11:01:06 +0100

    Main menu added

---
+++ Assets/Scripts/MenuManager.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuManager : MonoBehaviour
+{
+    public void Play(){
+        Debug.Log("Playing");
+        SceneManager.LoadScene(1);
+    }
+    public void Quit(){
+        Debug.Log("Quitting");
+        Application.Quit();
+    }
+
+}
