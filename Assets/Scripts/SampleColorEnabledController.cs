25fcc4390368d47de395b090b8c4ca4ef4714bba
Piotr Madura <peter@k4rasudev.me>
2023-03-18 12:21:17 +0100

    Sample color enabled controller
    Took 5 minutes

---
+++ Assets/Scripts/SampleColorEnabledController.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+public class SampleColorEnabledController : MonoBehaviour
+{
+    public ColorDetector colorDetector;
+
+    private SpriteRenderer[] _spriteRenderers;
+    
+    private void Start()
+    {
+        _spriteRenderers = GetComponentsInChildren<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        if (!colorDetector.isColorDetected) return;
+        foreach (var spriteRenderer in _spriteRenderers)
+        {
+            spriteRenderer.color = CharacterLightController.CurrentColor;
+        }
+    }
+}
