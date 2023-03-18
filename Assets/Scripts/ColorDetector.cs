7f78e620161a6a01147268dc172e6421ae8a68b2
Emilia Glibowska <emilia.glibowska@elmark.com.pl>
2023-03-18 22:23:22 +0100

    PREFABS

--- Assets/Scripts/ColorDetector.cs
+++ Assets/Scripts/ColorDetector.cs
@@ -7,14 +7,14 @@ public class ColorDetector : MonoBehaviour
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (!other.gameObject.CompareTag("EffectLight")) return;
-        Debug.Log("Color Detected");
+        // Debug.Log("Color Detected");
         isColorDetected = true;
     }
 
     private void OnTriggerExit2D(Collider2D other)
     {
         if (!other.gameObject.CompareTag("EffectLight")) return;
-        Debug.Log("Color Lost");
+        // Debug.Log("Color Lost");
         isColorDetected = false;
     }
 }
