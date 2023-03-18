afb323986ab7766d7c2c9a5222de2ba697ade432
DyskoPL <pawlrak@outlook.com>
2023-03-18 18:27:47 +0100

    Lights

--- Assets/Scripts/EffectLight.cs
+++ Assets/Scripts/EffectLight.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 
 public class EffectLight : MonoBehaviour
@@ -6,15 +7,30 @@ public class EffectLight : MonoBehaviour
     public Camera cameraRef;
     public SpriteRenderer spriteRef;
     public CharacterLightController lightRef;
+    public GameObject lightObject;
 
     // Update is called once per frame
+    private void Start()
+    {
+        spriteRef = GetComponent<SpriteRenderer>();
+    }
+
     private void Update()
     {
         if(Time.timeScale == 0f)
             return;
         // Stick the position to the mouse cursor
-        Vector2 mousePosition = cameraRef.ScreenToWorldPoint(Input.mousePosition);
-        transform.position = new Vector3(mousePosition.x, mousePosition.y, 0);
-        spriteRef.color = CharacterLightController.CurrentColor;
+        if (cameraRef != null)
+        {
+            Vector2 mousePosition = cameraRef.ScreenToWorldPoint(Input.mousePosition);
+            transform.position = new Vector3(mousePosition.x, mousePosition.y, 0);
+            spriteRef.color = CharacterLightController.CurrentColor;
+        }
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            var light = Instantiate(lightObject, transform.position, Quaternion.identity);
+            light.GetComponent<SpriteRenderer>().color = CharacterLightController.CurrentColor;
+        }
     }
 }
